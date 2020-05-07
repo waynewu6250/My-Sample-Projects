27b6fc57549c3cc302ba90dd65971195bc399af4
waynewu6250 <waynewu6250@gmail.com>
2020-05-07 18:38:41 -0400

    update

--- 4.CV/cellspectra/results/postprocess.py
+++ 4.CV/cellspectra/results/postprocess.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+import os
 
 # Run the file to obtain the data
 
@@ -8,6 +9,10 @@ def postprocess():
 
     with open('./results.pkl', 'rb') as f:
         results = pickle.load(f)
+    with open('./results_dcec_5.pkl', 'rb') as f:
+        labels1_dcec_5 = pickle.load(f)
+    with open('./results_dcec_7.pkl', 'rb') as f:
+        labels1_dcec_7 = pickle.load(f)
     
     labels1_kmeans =  results['image1_kmeans']
     labels2_kmeans =  results['image2_kmeans']
@@ -23,15 +28,55 @@ def postprocess():
     print("Deep Embedding Clustering results for image1 with shape: ", labels1_dcec.shape)
     print("Deep Embedding Clustering for image2 with shape:         ", labels2_dcec.shape)
 
-    # Example
+    if not os.path.exists('example/'):
+        os.makedirs('example/')
+    
+    # Example for kmeans
     # Cluster Number 5:
-    clusters = labels1_kmeans[0]
-    # You could set any number which is not cluster number 0 to some random value like -10
-    clusters[clusters!=0] = -10
-    # clusters[clusters!=0] = 1
-    plt.imshow(clusters)
-    plt.savefig('example.jpg')
-    plt.show()
+    for i in range(5):
+        clusters = labels1_kmeans[0].copy()
+        clusters[clusters!=i] = -100
+        plt.imshow(clusters)
+        plt.savefig('example/example_5_{}.jpg'.format(i+1))
+        plt.show()
+    # Cluster Number 6:
+    for i in range(6):
+        clusters = labels1_kmeans[1].copy()
+        clusters[clusters!=i] = -100
+        plt.imshow(clusters)
+        plt.savefig('example/example_6_{}.jpg'.format(i+1))
+        plt.show()
+    # Cluster Number 7:
+    for i in range(7):
+        clusters = labels1_kmeans[2].copy()
+        clusters[clusters!=i] = -100
+        plt.imshow(clusters)
+        plt.savefig('example/example_7_{}.jpg'.format(i+1))
+        plt.show()
+    
+    # Example for dcec
+    # Cluster Number 5:
+    
+    for i in range(5):
+        clusters = labels1_dcec_5['image1_dcec'].copy()
+        clusters[clusters!=i] = -100
+        plt.imshow(clusters)
+        plt.savefig('example/dcec_example_5_{}.jpg'.format(i+1))
+        plt.show()
+    # Cluster Number 6:
+    for i in range(6):
+        clusters = labels1_dcec.copy()
+        clusters[clusters!=i] = -100
+        plt.imshow(clusters)
+        plt.savefig('example/dcec_example_6_{}.jpg'.format(i+1))
+        plt.show()
+    # Cluster Number 7:
+    for i in range(7):
+        clusters = labels1_dcec_7['image1_dcec'].copy()
+        clusters[clusters!=i] = -100
+        plt.imshow(clusters)
+        plt.savefig('example/dcec_example_7_{}.jpg'.format(i+1))
+        plt.show()
 
 if __name__ == '__main__':
     postprocess()
