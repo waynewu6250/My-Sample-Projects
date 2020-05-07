1edd08211595fa24bfc14d450edbb2dbefe1b9e4
waynewu6250 <waynewu6250@gmail.com>
2020-05-07 11:15:03 -0400

    add

--- 4.CV/cellspectra/results/postprocess.py
+++ 4.CV/cellspectra/results/postprocess.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+import matplotlib.pyplot as plt
 
 # Run the file to obtain the data
 
@@ -21,6 +22,16 @@ def postprocess():
     print("Kmeans results for image2 with shape:                  ", labels2_kmeans.shape)
     print("Deep Embedding Clustering results for image1 with shape: ", labels1_dcec.shape)
     print("Deep Embedding Clustering for image2 with shape:         ", labels2_dcec.shape)
+
+    # Example
+    # Cluster Number 5:
+    clusters = labels1_kmeans[0]
+    # You could set any number which is not cluster number 0 to some random value like -10
+    clusters[clusters!=0] = -10
+    # clusters[clusters!=0] = 1
+    plt.imshow(clusters)
+    plt.savefig('example.jpg')
+    plt.show()
 
 if __name__ == '__main__':
     postprocess()
